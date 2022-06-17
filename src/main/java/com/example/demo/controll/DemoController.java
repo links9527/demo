e9d88eec02cf6a875e88917e72129c252206f861
LianKeShang <978771546@qq.com>
2022-06-17 17:47:03 +0800

    コメント

--- src/main/java/com/example/demo/controll/DemoController.java
+++ src/main/java/com/example/demo/controll/DemoController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class DemoControll {
+public class DemoController {
 	  @RequestMapping("/hello")
 	  public String hello(Model model) {
 	    model.addAttribute("hello", "Hello World!");
-	    return "demo";
+	    return "hello";
 	  }
 
 }
