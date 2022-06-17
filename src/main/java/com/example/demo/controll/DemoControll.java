0126003bc3baf88b6104631638e6c6093b072699
LianKeShang <978771546@qq.com>
2022-06-17 16:54:35 +0800

    コメント

---
+++ src/main/java/com/example/demo/controll/DemoControll.java
@@ -0,0 +1,15 @@
+package com.example.demo.controll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoControll {
+	  @RequestMapping("/hello")
+	  public String hello(Model model) {
+	    model.addAttribute("hello", "Hello World!");
+	    return "demo";
+	  }
+
+}
